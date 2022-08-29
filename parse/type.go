5777ab5714bf90cff144a206ec91d316fa4a2375
rol <rol@thooh.com>
2022-08-29 11:00:56 +0800

    feat(util): interface to string

--- parse/type.go
+++ parse/type.go
@@ -1,6 +1,10 @@
 package parse
 
-import "time"
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
 
 /*** Parse ***/
 
@@ -85,4 +89,57 @@ func Uint64ToStr(i uint64) (s string, err error) {
 // Uint64ToByteArr uint64 to []byte
 func Uint64ToByteArr(i uint64) (bytes []byte, err error) {
 	return bytes, convertAssign(&bytes, i)
+}
+
+// StrVal interface to string
+func StrVal(value interface{}) (key string) {
+	if value == nil {
+		return
+	}
+	switch value.(type) {
+	case float64:
+		ft := value.(float64)
+		key = strconv.FormatFloat(ft, 'f', -1, 64)
+	case float32:
+		ft := value.(float32)
+		key = strconv.FormatFloat(float64(ft), 'f', -1, 64)
+	case int:
+		it := value.(int)
+		key = strconv.Itoa(it)
+	case uint:
+		it := value.(uint)
+		key = strconv.Itoa(int(it))
+	case int8:
+		it := value.(int8)
+		key = strconv.Itoa(int(it))
+	case uint8:
+		it := value.(uint8)
+		key = strconv.Itoa(int(it))
+	case int16:
+		it := value.(int16)
+		key = strconv.Itoa(int(it))
+	case uint16:
+		it := value.(uint16)
+		key = strconv.Itoa(int(it))
+	case int32:
+		it := value.(int32)
+		key = strconv.Itoa(int(it))
+	case uint32:
+		it := value.(uint32)
+		key = strconv.Itoa(int(it))
+	case int64:
+		it := value.(int64)
+		key = strconv.FormatInt(it, 10)
+	case uint64:
+		it := value.(uint64)
+		key = strconv.FormatUint(it, 10)
+	case string:
+		key = value.(string)
+	case []byte:
+		key = string(value.([]byte))
+	default:
+		newValue, _ := json.Marshal(value)
+		key = string(newValue)
+	}
+	return
 }
